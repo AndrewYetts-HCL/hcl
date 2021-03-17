afddabe7099c65ec34b608807f38e06d2a137709
ayettshcl <andrew.yetts@hcl.com>
2021-03-17 09:30:08 -0500

    Added API's.

---
+++ Phase 2 Project/InventoryManager/InventoryManager.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using InventoryManager.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Xunit;
+
+namespace InventoryManager.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void AboutTest()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+
+
+
+    }
+}
---
+++ Phase 2 Project/InventoryManager/InventoryManager.Tests/Controllers/InventoryItemsControllerTests.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+
+namespace InventoryManager.Tests
+{
+    public class InventoryManagerControllerTests
+    {
+        [Fact]
+        public void CreateTest()
+        {
+
+
+        }
+
+
+
+    }
+}
