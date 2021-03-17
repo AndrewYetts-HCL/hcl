38a8d4ec3c60fad6a95d8c3ccc951609a4760872
ayettshcl <andrew.yetts@hcl.com>
2021-03-17 15:14:23 -0500

    Added unit tests for some of the CRUD functionalities.

--- Phase 2 Project/InventoryManager/InventoryManager.Tests/Controllers/InventoryItemsControllerTests.cs
+++ Phase 2 Project/InventoryManager/InventoryManager.Tests/Controllers/InventoryItemsControllerTests.cs
@@ -1,3 +1,7 @@
+using InventoryManager.Data;
+using InventoryManager.Models;
+using InventoryManager.Controllers;
+using Microsoft.EntityFrameworkCore;
 using System;
 using Xunit;
 
@@ -6,13 +10,60 @@ namespace InventoryManager.Tests
     public class InventoryManagerControllerTests
     {
         [Fact]
-        public void CreateTest()
+        public void IndexTest()
         {
+            // Arrange
+            InventoryItemsController controller = GetTestController();
 
+            // Act
+            var result = controller.Index("item");
 
+            // Assert
+            Assert.NotNull(result);
         }
 
+        [Fact]
+        public void DetailsTest()
+        {
+            // Arrange
+            InventoryItemsController controller = GetTestController();
 
+            // Act
+            var result = controller.Details(1);
 
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void DeleteTest()
+        {
+            // Arrange
+            InventoryItemsController controller = GetTestController();
+
+            // Act
+            var result = controller.Delete(1);
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        private InventoryItemsController GetTestController()
+        {
+            InventoryItemsController controller;
+
+            var options = new DbContextOptionsBuilder<InventoryManagerContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            using (var context = new InventoryManagerContext(options))
+            {
+                context.InventoryItem.Add(new InventoryItem());
+                context.SaveChanges();
+                controller = new InventoryItemsController(context);
+            }
+
+            return controller;
+        }
     }
 }
