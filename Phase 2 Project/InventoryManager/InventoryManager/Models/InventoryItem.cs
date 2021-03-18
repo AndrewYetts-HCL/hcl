0b46a42cb42b8665b01303561661434c5cdf97c0
ayettshcl <andrew.yetts@hcl.com>
2021-03-18 16:23:30 -0500

    Improved the UI. Added more data validation.

--- Phase 2 Project/InventoryManager/InventoryManager/Models/InventoryItem.cs
+++ Phase 2 Project/InventoryManager/InventoryManager/Models/InventoryItem.cs
@@ -7,12 +7,18 @@ namespace InventoryManager.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(maximumLength: 20)]
         public string Name { get; set; }
 
+        [StringLength(maximumLength: 50)]
         public string Description { get; set; }
 
+        [StringLength(maximumLength: 20)]
         public string Category { get; set; }
 
+        [Required]
+        [Range(0, 1000)]
         public int Quantity { get; set; }
 
         [DataType(DataType.Date)]
