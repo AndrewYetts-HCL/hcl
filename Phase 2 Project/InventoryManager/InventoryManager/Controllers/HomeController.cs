632a000a7d41a2a909f7133edfefea319183e3ec
ayettshcl <andrew.yetts@hcl.com>
2021-03-19 10:27:42 -0500

    Removed files that were not needed.

--- Phase 2 Project/InventoryManager/InventoryManager/Controllers/HomeController.cs
+++ Phase 2 Project/InventoryManager/InventoryManager/Controllers/HomeController.cs
@@ -10,11 +10,5 @@ namespace InventoryManager.Controllers
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
--- Phase 2 Project/InventoryManager/InventoryManager/Controllers/ValuesController.cs
+++ Phase 2 Project/InventoryManager/InventoryManager/Controllers/ValuesController.cs
@@ -23,9 +23,7 @@ namespace InventoryManager.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<InventoryItem>>> GetInventoryItem()
         {
-            var list = await _context.InventoryItem.ToListAsync();
-            var json = list.ToString();
-            return list;
+            return await _context.InventoryItem.ToListAsync();
         }
 
         // GET: api/Values/5
@@ -40,71 +38,6 @@ namespace InventoryManager.Controllers
             }
 
             return inventoryItem;
-        }
-
-        // PUT: api/Values/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutInventoryItem(int id, InventoryItem inventoryItem)
-        {
-            if (id != inventoryItem.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(inventoryItem).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!InventoryItemExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Values
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPost]
-        public async Task<ActionResult<InventoryItem>> PostInventoryItem(InventoryItem inventoryItem)
-        {
-            _context.InventoryItem.Add(inventoryItem);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetInventoryItem", new { id = inventoryItem.Id }, inventoryItem);
-        }
-
-        // DELETE: api/Values/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<InventoryItem>> DeleteInventoryItem(int id)
-        {
-            var inventoryItem = await _context.InventoryItem.FindAsync(id);
-            if (inventoryItem == null)
-            {
-                return NotFound();
-            }
-
-            _context.InventoryItem.Remove(inventoryItem);
-            await _context.SaveChangesAsync();
-
-            return inventoryItem;
-        }
-
-        private bool InventoryItemExists(int id)
-        {
-            return _context.InventoryItem.Any(e => e.Id == id);
         }
     }
 }
